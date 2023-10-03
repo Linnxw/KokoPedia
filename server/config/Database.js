cbe55ae38fbb1dd7db9464b3ffd32ba80800dcbf
Linnxw <ulin230606@gmail.com>
2023-10-03 22:40:53 +0700

    edit db

--- server/config/Database.js
+++ server/config/Database.js
@@ -1,10 +1,10 @@
 import mysql from "mysql"
 
 const db = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'root',
-  database:'penjualan'
+  host:process.env.HOST || 'localhost',
+  user:process.env.USER || 'root',
+  password:process.env.PASSWORD || 'root',
+  database:process.env.DATABASE || 'penjualan'
 })
 
 export default db
