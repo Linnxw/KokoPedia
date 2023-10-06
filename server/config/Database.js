34d66f2d1c631b5767cf29280aba0b2ae1515616
Linnxw <ulin230606@gmail.com>
2023-10-06 20:30:22 +0700

    env

--- server/config/Database.js
+++ server/config/Database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql")
 
 const db = mysql.createConnection({
-  host:process.env.HOST || 'linnxw.my.id',
-  user:process.env.USER || 'linnxwmy_penjualan',
-  password:process.env.PASSWORD || 'linnxw230606060623',
-  database:process.env.DATABASE || 'linnxwmy_penjualan'
+  host:process.env.HOST || 'localhost',
+  user:process.env.USER || 'root',
+  password:process.env.PASSWORD || 'root',
+  database:process.env.DATABASE || 'penjualan'
 })
 
 module.exports =  db
